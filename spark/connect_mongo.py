2a230a80775cea23c7f7229b5397714ecbbd7ffc
wugang <752820344@qq.com>
2019-04-28 11:17:08 +0800

    [增加]spark sql 连接mongo

---
+++ spark/connect_mongo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : connect_mongo.py
+# @Author: wu gang
+# @Date  : 2019/4/28
+# @Desc  : 
+# @Contact: 752820344@qq.com
+
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    mongodb_url = "mongodb://search:search%40huoli123@123.56.222.127/tips."
+    spark = SparkSession \
+        .builder \
+        .appName("Connect MongoDB") \
+        .config("spark.mongodb.input.uri", mongodb_url + "carBasicData") \
+        .config("spark.mongodb.output.uri", mongodb_url + "carBasicData") \
+        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.11:2.2.0")\
+        .getOrCreate()
+    df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+    df.show(truncate=False)
